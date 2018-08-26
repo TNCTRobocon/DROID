c3ee6fd504054c175551e4c4a88d625cc0f0cbfc
keimoriyama <keischwiiz@gmail.com>
2018-08-26 20:47:21 +0900

    余計なファイルの追跡を削除

--- build/simulator/production/Driver/adc.o.d
+++
@@ -1,6 +0,0 @@
-build/simulator/production/Driver/adc.o:  \
-Driver/adc.c  \
-Driver/adc.h  \
- Driver/../Helper/dsp_helper.h  \
-Driver/../Setting/configuration.h  \
- Driver/../Setting/p30F4012.h 
